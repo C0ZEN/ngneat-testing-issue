06bd4defe8b175ea452aba502a551c07930f7168
C0ZEN <geoffrey.testelin@gmail.com>
2021-04-20 14:21:35 +0200

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormControl } from "@ngneat/reactive-forms";
+interface IForm {
+  firstname: string;
+}
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  public form = new FormGroup({
-    name: new FormControl("", Validators.required)
+  public form = new FormGroup<IForm>({
+    firstname: new FormControl<string>("", Validators.required)
   });
 }
